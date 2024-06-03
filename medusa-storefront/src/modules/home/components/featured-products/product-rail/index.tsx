409eda4eb5b15f312fd9942a01cc58e7996d258f
DEF4LT-303 <ryanrafi72@gmail.com>
2024-06-03 15:44:29 +0600

    footer fixed

--- medusa-storefront/src/modules/home/components/featured-products/product-rail/index.tsx
+++ medusa-storefront/src/modules/home/components/featured-products/product-rail/index.tsx
@@ -1,21 +1,21 @@
-import { Region } from "@medusajs/medusa"
-import { Text } from "@medusajs/ui"
+import { Region } from "@medusajs/medusa";
+import { Text } from "@medusajs/ui";
 
-import InteractiveLink from "@modules/common/components/interactive-link"
-import ProductPreview from "@modules/products/components/product-preview"
-import { ProductCollectionWithPreviews } from "types/global"
+import InteractiveLink from "@modules/common/components/interactive-link";
+import ProductPreview from "@modules/products/components/product-preview";
+import { ProductCollectionWithPreviews } from "types/global";
 
 export default function ProductRail({
   collection,
   region,
 }: {
-  collection: ProductCollectionWithPreviews
-  region: Region
+  collection: ProductCollectionWithPreviews;
+  region: Region;
 }) {
-  const { products } = collection
+  const { products } = collection;
 
   if (!products) {
-    return null
+    return null;
   }
 
   return (
@@ -39,5 +39,5 @@ export default function ProductRail({
           ))}
       </ul>
     </div>
-  )
+  );
 }
