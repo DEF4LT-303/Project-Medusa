db5a284f89c7af4e27b938e1f7943d3bf35f7f14
DEF4LT-303 <ryanrafi72@gmail.com>
2024-06-07 20:49:05 +0600

    product page update

--- next-store/src/components/product/cards/product-info.tsx
+++ next-store/src/components/product/cards/product-info.tsx
@@ -8,36 +8,48 @@ import { StoreProductsRes } from "@medusajs/medusa";
 
 const ProductInfo = ({ product }: StoreProductsRes) => {
   return (
-    <Accordion type="single" collapsible className="w-full p-2 md:w-1/2">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>About the product</AccordionTrigger>
-        <AccordionContent>
-          <p className="text-xl">{product.title}</p>
-          <br />
-          <p>
-            Description: {product?.description || "No description available"}
-          </p>
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Variants</AccordionTrigger>
-        <AccordionContent className="flex justify-between flex-wrap">
-          {product.variants.map((variant) => (
-            <div key={variant.id}>
-              <p>{variant.title}</p>
-
-              <p>{variant.prices[0].amount}</p>
+    <div className="w-full p-2 md:w-1/2 flex flex-col gap-3">
+      <div className="flex flex-col gap-2">
+        <p className="text-2xl font-bold underline">About he product:</p>
+        <div className="flex flex-col gap-5">
+          <p className="text-lg">{product?.title || "-"}</p>
+          <p className="text-sm">{product?.description || "-"}</p>
+        </div>
+      </div>
+      <Accordion type="single" collapsible>
+        <AccordionItem value="item-2">
+          <AccordionTrigger>Variants</AccordionTrigger>
+          <AccordionContent className="grid grid-cols-2 justify-start gap-5">
+            <div>
+              <p>Weight</p>
+              <p>{product?.weight || "-"}</p>
             </div>
-          ))}
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>Refund Policy</AccordionTrigger>
-        <AccordionContent>
-          All products are refundable within 7-days of purchase.
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+            <div>
+              <p>Length</p>
+              <p>{product?.length || "-"}</p>
+            </div>
+            <div>
+              <p>Height</p>
+              <p>{product?.height || "-"}</p>
+            </div>
+            <div>
+              <p>Width</p>
+              <p>{product?.width || "-"}</p>
+            </div>
+            <div>
+              <p>Material</p>
+              <p>{product?.material || "-"}</p>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+        <AccordionItem value="item-3">
+          <AccordionTrigger>Refund Policy</AccordionTrigger>
+          <AccordionContent>
+            All products are refundable within 7-days of purchase.
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
+    </div>
   );
 };
 
